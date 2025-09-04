c9767a25a42e498377a35c8b9bfe6c5aae9d55a3
radhasaik2 <radhasaik.2@gmail.com>
2025-09-04 08:43:51 +0530

    Databaseconnection.java

--- Databaseconnection.java
+++ Databaseconnection.java
@@ -1,22 +1,38 @@
 package in.todoservlet;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.sun.jdi.connect.spi.Connection;
+public class Tododbcon {
 
-public class Databaseconnection {
-	 private static final String URL = "jdbc:mysql://localhost:3306/your_database_name";
-	    private static final String USER = "todoitems";
-	    private static final String PASSWORD = "root";
-	    static {
-	        try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	        } catch (ClassNotFoundException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	    public static Connection getConnection() throws SQLException {
-	        return (Connection) DriverManager.getConnection(URL, USER, PASSWORD);
-	    }
+    private static final String URL = "jdbc:mysql://localhost:3306/todoitems";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public boolean insertTodoItem(Todoitemsmain todo) {
+        String query = "INSERT INTO todoitems (currentDate, todoDesc, targetDate, todoStatus, credited_by, credited_date) VALUES (?, ?, ?, ?, ?, NEW())";
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setTimestamp(1, new Timestamp(todo.getCurrentDate().getTime()));
+            ps.setString(2, todo.getTodoDesc());
+            ps.setTimestamp(3, new Timestamp(todo.getTargetDate().getTime()));
+            ps.setString(4, todo.getTodoStatus());
+            ps.setString(5, "admin"); 
+
+            int rows = ps.executeUpdate();
+            return rows > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
