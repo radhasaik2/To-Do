e74113203ff7a490794f9b0c3c5bc4fb74dc7839
radhasaik2 <radhasaik.2@gmail.com>
2025-09-19 21:25:21 +0530

    Login-Credentials Assignment1dao.java

--- Assignment1/Assignment1dao.java
+++ Assignment1/Assignment1dao.java
@@ -3,27 +3,89 @@ package in.spring.wmvc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-	public class Assignment1dao {
-	private static final String URL = "jdbc:mysql://localhost:3306/todo_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+public class Assignment1dao {
+    private static final String url = "jdbc:mysql://localhost:3306/users";
+    private static final String user = "root";
+    private static final String password = "root";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url,user,password);
     }
     
     public static int insertUsers(Userdto dao) throws Exception {
-        String query = "INSERT INTO users (name,email,password) VALUES (?,?,?)";
+        if (isEmailExists(dao.getEmail())) {
+            throw new Exception("Email already exists");
+        }
+        if (isNameExists(dao.getName())) {
+            throw new Exception("Username already exists");
+        }
+        
+        String query = "insert into users(name, email, password) values(?, ?, ?)";
         try (Connection con = getConnection();
-             PreparedStatement ps = con.prepareStatement(query)) {
+             PreparedStatement ps = con.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, dao.getName());
             ps.setString(2, dao.getEmail());
-            ps.setString(3, dao.getPassword()); 
-            return ps.executeUpdate();
+            ps.setString(3, Passwordinfo.hashPassword(dao.getPassword()));
+            int Rows = ps.executeUpdate();
+            
+            if (Rows > 0) {
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        return rs.getInt(1); 
+                    }
+                }
+            }
+            return 0;
         }
     }
     
+    public static Userdto getUserByEmail(String email) throws Exception {
+        String query = "select userid, name, email, password from users where email = ?";
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            ps.setString(1, email);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    Userdto user = new Userdto();
+                    user.setUserid(String.valueOf(rs.getInt("userid")));
+                    user.setName(rs.getString("name"));
+                    user.setEmail(rs.getString("email"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
     
+    public static boolean isEmailExists(String email) throws Exception {
+        String query = "select count(*) from users where email = ?";
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            ps.setString(1, email);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt(1) > 0;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static boolean isNameExists(String name) throws Exception {
+        String query = "select count(*) from users where name = ?";
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            ps.setString(1, name);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt(1) > 0;
+                }
+            }
+        }
+        return false;
+    }
 }
