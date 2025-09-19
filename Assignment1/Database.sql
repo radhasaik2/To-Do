cc2b58635f0c07562ac932cf223423b690ab0a94
radhasaik2 <radhasaik.2@gmail.com>
2025-09-19 21:38:16 +0530

    Login-Credentials Database.sql

--- Assignment1/Database.sql
+++ Assignment1/Database.sql
@@ -15,28 +15,3 @@ select * from users;
 
 alter table users modify password varchar(60);
 alter table users modify email varchar(60);
-
-
--- Create movies table
-create table movies (
-    movieid int primary key auto_increment,
-    title varchar(100) not null,
-    description text,
-    release_year int
-);
-
-insert into movies (title, narration, release_year) values
-('The Dark Knight', 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 2008);
-
--- Create ratings table
-create table ratings (
-ratingid int primary key auto_increment,
-userid int not null,
-movieid int not null,
-rating int not null check(rating between 1 and 5),
-created_at date default (current_date()),
-foreign key (userid) references users(userid),
-foreign key (movieid) references movies(movieid)
-);
-
-select * from ratings;
