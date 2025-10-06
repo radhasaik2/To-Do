7ae3c70c02b3e7f143e6893988c53f0409433ff2
radhasaik2 <radhasaik.2@gmail.com>
2025-10-06 12:56:17 +0530

    Springmvc - task1

---
+++ MovieTicket/Springmvc/Entity.java
@@ -0,0 +1,33 @@
+package in.spring.mvc;
+
+public class Entity {
+	public int userid;
+	public String username;
+	public String password;
+	public String email;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
